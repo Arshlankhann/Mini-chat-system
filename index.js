c2b7a63caa54eb88d598f468a916a21207c0b008
Arshlankhann <arshlank894@gmail.com>
2024-12-01 13:17:25 +0530

    c1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override")
+const ExpressError = require("./ExpressError.js")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -22,14 +23,20 @@ async function main() {
 }
 
 //index route
+
 app.get("/chats", async (req, res) => {
-    let chats = await Chat.find();
-    // console.log(chats);
-    res.render("index.ejs", { chats })
+    try {
+        let chats = await Chat.find();
+        // console.log(chats);
+        res.render("index.ejs", { chats })
+    } catch (err) {
+        next(err);
+    }
 })
 
 //new route
-app.get("/chats/new", (req, res) => {
+app.get("/chats/new",(req, res) => {
+    new ExpressError(404, "Page not found")
     res.render("new.ejs");
 })
 
@@ -54,32 +61,67 @@ app.post("/chats", (req, res) => {
     res.redirect("/chats");
 })
 
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>next(err));
+    }
+}
+
+
+// NEW - show Route
+app.get("/chats/:id", asyncWrap(async (req, res) => {
+   
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        if (!chat) {
+            next(new ExpressError(404, "Chat not found"));
+        }
+        res.render("edit.ejs", { chat })
+    } 
+))
+
 //Edit Route
-app.get("/chats/:id/edit", async (req, res) => {
-    let { id } = req.params;
-    let chat = await Chat.findById(id)
-    res.render("edit.ejs", { chat });
-})
+app.get("/chats/:id/edit", asyncWrap(async (req, res) => {
+
+        let { id } = req.params;
+        let chat = await Chat.findById(id)
+        res.render("edit.ejs", { chat });
+    } 
+))
 
 //update route
 app.put("/chats/:id", async (req, res) => {
-    let { id } = req.params;
-    let { msg: newmsg } = req.body;
-    console.log(newmsg);
-    let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newmsg }, { runValidators: true, new: true });
-    console.log(updatedChat);
-    res.redirect("/chats");
+    try {
+        let { id } = req.params;
+        let { msg: newmsg } = req.body;
+        console.log(newmsg);
+        let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newmsg }, { runValidators: true, new: true });
+        console.log(updatedChat);
+        res.redirect("/chats");
+    } catch (err) {
+       next(err)
+    }
 })
 //destroy route
-app.delete("/chats/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedChat=await Chat.findByIdAndDelete(id);
-    console.log(deletedChat);
-    res.redirect("/chats");
+app.delete("/chats/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(deletedChat);
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
 })
 
 app.get("/", (req, res) => {
     res.send("Root is working !");
+})
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Some error Occured" } = err;
+    res.status(status).send(message);
 })
 
 app.listen(8080, () => {
--- init.js
+++ init.js
@@ -7,7 +7,7 @@ main()
     .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp')
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp')
 }
 
 let allChats = [
